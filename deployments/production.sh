75a4f7b105ae067a5efac3775a5ad6896ad53011
nicolas <nicolasrestrepo34@gmail.com>
2018-05-14 22:37:55 -0500

    add token in now

--- deployments/production.sh
+++ deployments/production.sh
@@ -1,4 +1,4 @@
 sudo npm i -g --unsafe-perm now
 echo "deploying production..."
 URL=$(now --docker --public -t $NOW_TOKEN)
-now alias $URL production-bogota-devops
+now alias -t $NOW_TOKEN $URL production-bogota-devops
